d60078e43f9b702b8c7e47a77ed58e2dbad24070
homma nonoka <e185730@ie.u-ryukyu.ac.jp>
2023-07-21 20:39:42 +0900

    add custom color

--- twind.config.ts
+++ twind.config.ts
@@ -2,4 +2,13 @@ import { Options } from "$fresh/plugins/twind.ts";
 
 export default {
   selfURL: import.meta.url,
+  theme: {
+    colors: {
+      MyViolet: "#525FE1", // カスタムカラーを定義
+      MyOrange: "#F86F03",
+      MyOrangeLighter:"#FFA41B", // 別のカスタムカラーを定義
+      MyWhite:"#FFF6F4",
+      // 他のカスタムカラーを必要に応じて追加
+    },
+  },
 } as Options;
