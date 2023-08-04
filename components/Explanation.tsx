00d580a0ae59e74bf84adeeb96f13985a38c3b76
homma nonoka <e185730@ie.u-ryukyu.ac.jp>
2023-08-04 21:48:17 +0900

    add

--- components/Explanation.tsx
+++ components/Explanation.tsx
@@ -3,18 +3,21 @@ type Props = {
     text: string;
   };
   
-export default function Explanation({text,is_input}:Props) {
+export default function Explanation({text,src}:Props) {
 
     return (
       <div
-      class="w-full flex px-8 py-10 min-h-[24em]  mt-5 bg-MyWhite
+      class="w-full px-8 py-10 min-h-[24em]  mt-5 bg-MyWhite
       rounded-xl border-solid border-4 border-MyOrange"
       >
         <div class="text-left left-0">
             <div class="text-3xl text-MyOrange underline ">
-            {text}</div>
-        </div>
-  
+            {text}</div>  
+            <div class="p-3">
+            <iframe src={src} frameborder="0" width="100%" height="569" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>        </div>
+            </div>
+
+
       </div>
     );
   }
--- routes/index.tsx
+++ routes/index.tsx
@@ -2,6 +2,8 @@ import { Head } from "$fresh/runtime.ts";
 
 import  Hero  from "../components/Hero.tsx";
 import Explanation from "../components/Explanation.tsx";
+import { tx } from "$fresh/plugins/twind.ts";
+
 export default function Home() {
 
   return (
@@ -9,7 +11,7 @@ export default function Home() {
       <Head>
         <title>お店屋さんごっこDX</title>
       </Head>
-      <div class="p-4 mx-auto max-w-screen-md">
+      <div class=" p-4 mx-auto max-w-screen-md">
         <img
           src="/logo.svg"
           class="absolute w-32 h-32"
@@ -19,8 +21,8 @@ export default function Home() {
       <div
       class="w-full justify-center">
         <Hero />
-        <Explanation text="QRコードの作り方"/>
-        <Explanation  text="レジスターの遊び方"/>
+        <Explanation text="QRコードの作り方" src="https://docs.google.com/presentation/d/e/2PACX-1vQ9SHQKjt0e0emW7_oidNAAKiUibuwJUVkXUc_2xlD1VCSQUSZpHFvWQrqSaIuKMtZWdedzg_LbprYq/embed?start=false&loop=false&delayms=3000"/>
+        <Explanation  text="レジスターの遊び方" src="https://docs.google.com/presentation/d/e/2PACX-1vQDq-dM7EbPVErasZfF5_Wmu7-0vue3a2vFodi4xNnpcEcsTBGbPwmg2YHnz-nD7prpoe_KpTjgBM7a/embed?start=false&loop=false&delayms=3000"/>
       </div>
 
       </div>
